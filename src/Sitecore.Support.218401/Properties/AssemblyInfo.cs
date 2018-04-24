5b4ca46c2140ea4cc0fd9fef56e9b15bf233d5cd
sc-ido <ido@sitecore.net>
2018-04-24 17:27:01 +0300

    Blank DLL as a bug marker

---
+++ src/Sitecore.Support.218401/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.218401")]
+[assembly: AssemblyProduct("Sitecore.Support.218401")]
+[assembly: ComVisible(false)]
